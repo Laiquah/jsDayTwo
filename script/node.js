a7861c8f57e4b44c30aa7f43f7422520a946cbe8
Laiquah <cotielaiquah@gmail.com>
2023-05-16 14:47:20 +0200

    added changes to node.js

--- script/node.js
+++ script/node.js
@@ -8,6 +8,12 @@ let output = Math.floor(subjects.length /2 );
 // console.log(output);
 console.log(subjects[output]);
 
+if ((subjects.length%2)==0) {
+    console.log(subjects[output-1]+' '+subjects[output])
+}else{
+    console.log(subjects[output]) 
+}
+
 // const myArray = ['apple', 'banana', 'orange', 'pear', 'watermelon'];
 // const middleValue = findMiddleValue(myArray);
 // console.log(middleValue); // Output: 'orange'
