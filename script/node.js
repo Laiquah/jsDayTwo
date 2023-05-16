4c3be1cc8c7205eab63eaff91fe8380e9951065d
Laiquah <cotielaiquah@gmail.com>
2023-05-16 14:34:57 +0200

    add changes to node.ja

---
+++ script/node.js
@@ -0,0 +1,13 @@
+let subjects = ['Python', 'C++', 'VB.Net', 'Javascript', 'Assembly'];
+// let lastElement = subjects.find[4]
+console.log(subjects.pop());
+// subjects.pop();
+// console.log(subjects.pop(2));
+
+let output = Math.floor(subjects.length /2 );
+// console.log(output);
+console.log(subjects[output]);
+
+// const myArray = ['apple', 'banana', 'orange', 'pear', 'watermelon'];
+// const middleValue = findMiddleValue(myArray);
+// console.log(middleValue); // Output: 'orange'
