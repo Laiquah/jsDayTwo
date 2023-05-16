ef6e79f5d5a01db51bfff5523d1b5916886039ce
Laiquah <cotielaiquah@gmail.com>
2023-05-16 14:50:05 +0200

    added changes to node.js

--- script/node.js
+++ script/node.js
@@ -1,11 +1,7 @@
 let subjects = ['Python', 'C++', 'VB.Net', 'Javascript', 'Assembly'];
-// let lastElement = subjects.find[4]
 console.log(subjects.pop());
-// subjects.pop();
-// console.log(subjects.pop(2));
 
 let output = Math.floor(subjects.length /2 );
-// console.log(output);
 console.log(subjects[output]);
 
 if ((subjects.length%2)==0) {
@@ -13,7 +9,3 @@ if ((subjects.length%2)==0) {
 }else{
     console.log(subjects[output]) 
 }
-
-// const myArray = ['apple', 'banana', 'orange', 'pear', 'watermelon'];
-// const middleValue = findMiddleValue(myArray);
-// console.log(middleValue); // Output: 'orange'
